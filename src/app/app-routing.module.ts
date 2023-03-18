d1a37aa913617b5be1ed58c04f23012a3bb2b6eb
Alizee42 <alizee.gueye@gmail.com>
2023-03-18 17:04:56 +0100

    refactorisation du code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,14 +6,7 @@ const routes: Routes = [
     path: '',
     loadChildren: () => import('./views/pages/main/main.module').then(m => m.MainModule)
   },
-  {
-    path: 'auth',
-    loadChildren: () => import('./views/pages/auth/auth.module').then(m => m.AuthModule)
-  },
-  { 
-    path: 'error',
-    loadChildren: () => import('./views/pages/error-page/error-page.module').then(m => m.ErrorPageModule)
-  },
+
   { path: '', redirectTo: '', pathMatch: 'full' },
   { path: '**', redirectTo: 'error', pathMatch: 'full' }
 ];
